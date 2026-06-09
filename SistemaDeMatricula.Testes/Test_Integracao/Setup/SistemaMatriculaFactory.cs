9e673db7306332b57584f5dc3d00d93267ba898e
gustavo-lima <zandergustavolara@gmail.com>
2026-06-08 23:25:15 -0400

    4

--- SistemaDeMatricula.Testes/Test_Integracao/Setup/SistemaMatriculaFactory.cs
+++ SistemaDeMatricula.Testes/Test_Integracao/Setup/SistemaMatriculaFactory.cs
@@ -13,19 +13,15 @@ public class SistemaMatriculaFactory : WebApplicationFactory<Program>, IAsyncLif
     {
         builder.ConfigureAppConfiguration((context, config) =>
         {
-            // 1. Limpa fontes de configuração para evitar conflito com arquivos locais
             config.Sources.Clear();
-
-            // 2. Adiciona variáveis de ambiente (onde o GitHub injeta a string)
             config.AddEnvironmentVariables();
-            builder.UseEnvironment("Development");
 
-            // 3. Opcional: Adiciona o json apenas se existir
+            builder.UseEnvironment("Testing");
+
             config.AddJsonFile("appsettings.json", optional: true);
 
             var settings = config.Build();
 
-            // 4. Força a leitura da string que injetamos
             var connectionString = settings["ConnectionStrings:DefaultConnection"];
 
             if (string.IsNullOrEmpty(connectionString))
@@ -48,7 +44,6 @@ public class SistemaMatriculaFactory : WebApplicationFactory<Program>, IAsyncLif
         using var scope = Services.CreateScope();
         var contexto = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-        // APAGA TUDO SÓ QUANDO TODOS OS TESTES TERMINAREM
         await contexto.Database.EnsureDeletedAsync();
     }
 }
