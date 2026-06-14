5a98b3fed744a3821ef103276467b3c52b280801
gustavo-lima <zandergustavolara@gmail.com>
2026-06-13 23:47:04 -0400

    fix(Identity):Identification system implemented in registration project.

---
+++ istemaDeMatricula.Identity.API/Data/AppIdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaDeMatricula.Identity.API.Data;
+
+public class AppIdentityDbContext : IdentityDbContext
+{
+    public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> Options) : base(Options)
+    {
+    }
+}
---
+++ istemaDeMatricula.Identity.API/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeMatricula.Identity.API.Data;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+var connectionString = builder.Configuration.GetConnectionString("IdentityConnection");
+
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AppIdentityDbContext>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapIdentityApi<IdentityUser>();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
